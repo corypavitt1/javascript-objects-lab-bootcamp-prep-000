347ae1646fec42922799ffa47825f1b58c63fd52
Cory Pavitt <corypavitt@hotmail.com>
2018-07-23 23:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 }
 
-function deleteFromObjec=tByKey(object, key) {
+function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({},object)
   delete newObj[key]
   return newObj
